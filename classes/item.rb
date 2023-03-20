eec2a4a79cc7e1f0ee2ba555e68055a45339a72f
PrangonGhose <prangonghose@gmail.com>
2023-03-21 02:24:00 +0600

    Update Id and Fix linters

--- classes/item.rb
+++ classes/item.rb
@@ -1,25 +1,23 @@
 class Item
-    def initialize(genre, author, source, label, publish_date)
-        @id = Random.rand(1..1000)
-        @genre = genre
-        @author = author
-        @source = source
-        @label = label
-        @publish_date = publish_date
-        @archived = false
-    end
+  def initialize(genre, author, source, label, publish_date)
+    @id = Time.now.to_i
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @archived = false
+  end
 
-    def move_to_archive(item)
-        if can_be_archived?(item)
-            item.archived = true
-        else
-            item.archived = false
-        end
-    end
+  def move_to_archive(item)
+    return unless can_be_archived?(item)
 
-    private
+    item.archived = true
+  end
 
-    def can_be_archived?(item)
-        item.publish_date > 10
-    end
+  private
+
+  def can_be_archived?(item)
+    item.publish_date > 10
+  end
 end
