6c31623fd58ac3fcafbab6bd91dd9f3978acf0e2
ShinhyoBelok <shibo6937@gmail.com>
2023-03-21 17:12:27 -0400

    Add label class

---
+++ classes/label.rb
@@ -0,0 +1,13 @@
+class Label
+    def initialize(title, color)
+        @id = Time.now.to_i
+        @title = title
+        @color = color
+        items = []
+    end
+
+    def add_item(item)
+        items << item
+        item.label = self
+    end
+end
