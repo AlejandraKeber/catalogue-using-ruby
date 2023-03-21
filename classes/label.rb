cbdc5467d7b67ef56cbcdccde9f08af55ae17296
ShinhyoBelok <shibo6937@gmail.com>
2023-03-21 18:04:26 -0400

    Add testing for label

--- classes/label.rb
+++ classes/label.rb
@@ -1,13 +1,18 @@
+require_relative 'item'
+
 class Label
+    attr_accessor :title, :color
+    attr_reader :items
+
     def initialize(title, color)
         @id = Time.now.to_i
         @title = title
         @color = color
-        items = []
+        @items = []
     end
 
     def add_item(item)
-        items << item
+        @items << item
         item.label = self
     end
 end
