90b06700292fda04f384e3269a80180edc7b3f06
ShinhyoBelok <shibo6937@gmail.com>
2023-03-21 13:18:39 -0400

    Add book class

---
+++ classes/book.rb
@@ -0,0 +1,17 @@
+require_relative 'item'
+
+class Book < Item
+  # rubocop:disable Metrics/ParameterLists
+  def initialize(genre, author, source, label, publish_date, publisher, cover_state)
+    # rubocop:enable Metrics/ParameterLists
+    super(genre, author, source, label, publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  private
+
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
+end
