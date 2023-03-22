d5574455e9057b16f32bd9b89c2e0130f86e2c88
PrangonGhose <prangonghose@gmail.com>
2023-03-23 03:00:54 +0600

    Add create_genre module

---
+++ classes/create_genre.rb
@@ -0,0 +1,36 @@
+require_relative 'genre'
+
+module CreateGenre
+  def show_genre(list_of_genre)
+    puts('Select genre from the list:')
+    list_all_genre(list_of_genre)
+    puts("#{list_of_genre.length + 1}. Create a new genre")
+    option = gets.chomp
+    genre = select_genre(list_of_genre, option.to_i)
+    genre
+  end
+
+  def list_all_genre(list_of_genre)
+    if list_of_genre.empty?
+      puts 'No genre to display. You can add one.'
+    else
+      list_of_genre.each_with_index { |genre, i| puts(" | #{i}. Genre: #{genre.name} | ") }
+    end
+  end
+
+  def select_genre(list_of_genre, index)
+    if index < list_of_genre.length
+      list_of_genre[index]
+    else
+      print('Enter the name of the genre: ')
+      name = gets.chomp
+      genre = new_genre(name)
+      genre
+    end
+  end
+
+  def new_genre(name)
+    genre = Genre.new(name)
+    genre
+  end
+end
