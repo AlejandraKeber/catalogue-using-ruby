ad034279e14fc355f79860270eaf57a5d44b9eec
Alejandra Keber <alejandrakeber@gmail.com>
2023-03-23 09:08:25 -0500

    Add reviewer suggestion

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,10 +1,11 @@
 CREATE TABLE item (
-    id SERIAL PRIMARY KEY,
+    id INT GENERATED ALWAYS AS IDENTITY,
     genre_id INT,
     author_id INT,
     label_id INT,
     publish_date DATE,
     archived BOOLEAN,
+    PRIMARY KEY (id),
     FOREIGN KEY (genre_id) REFERENCES genre(id),
     FOREIGN KEY (author_id) REFERENCES author(id),
     FOREIGN KEY (label_id) REFERENCES label(id)
