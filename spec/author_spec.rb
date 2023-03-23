75a3597ec9fafb3d6cb93b43e163aaea224058d9
AlejandraKeber <alejandrakeber@gmail.com>
2023-03-23 18:35:35 -0500

    Solve merge conflict

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -14,10 +14,4 @@ describe 'Test Author class' do
   it "author last name should be 'Keber'" do
     expect(author.last_name).to eql 'Keber'
   end
-
-  it '#add_item adds self to item as well' do
-    item = Item.new(publish_date: '2020-10-10')
-    author.add_item(item)
-    expect(autor.items.include?(item)).to be true
-  end
 end
