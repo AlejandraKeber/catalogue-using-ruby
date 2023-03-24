9c4a9768ae76cd8cb3652a897779de870e8af788
ShinhyoBelok <shibo6937@gmail.com>
2023-03-23 22:58:58 -0400

    Fix linters

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -4,7 +4,7 @@ require_relative '../classes/book'
 describe Book do
   context 'When testing the Book Class' do
     it 'should create a book object with a item and their properties' do
-      book = Book.new('1996-08-01', 'Bantam Spectra','A game of thrones')
+      book = Book.new('1996-08-01', 'Bantam Spectra', 'A game of thrones')
       expect(book).to be_instance_of(Book)
       expect(book.publish_date).to eq Date.parse('1996-08-01')
       expect(book.publisher).to eq 'Bantam Spectra'
